eab265912245e4ab51076b9534796a3f606025ca
unknown <nachomontiel16@gmail.com>
2022-09-07 23:26:29 -0300

    02-JS-I

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -4,10 +4,10 @@
 const nuevaString = 'HOLA';
 
 // Crea una variable numérica, puede ser cualquier número:
-const nuevoNum = '12';
+const nuevoNum = 12;
 
 // Crea una variable booleana:
-const nuevoBool = 'true';
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
@@ -33,28 +33,28 @@ function devolverString(str) {
 function suma(x, y) {
   // "x" e "y" son números
   // Suma "x" e "y" juntos y devuelve el valor
-  // Tu código: x + y;
+  // Tu código: return x + y;
 }
 
 function resta(x, y) {
   // Resta "x" de "y" y devuelve el valor
-  // Tu código: x - y;
+  // Tu código: return x - y;
 }
 
 function multiplica(x, y) {
   // Multiplica "x" por "y" y devuelve el valor
-  // Tu código: x * y;
+  // Tu código: return x * y;
 }
 
 function divide(x, y) {
   // Divide "x" entre "y" y devuelve el valor
-  // Tu código: x / y;
+  // Tu código: return x / y;
 }
 
 function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
-  // Tu código: if(x===y) {true;}  else {false;}
+  // Tu código: if(x===y) {return true;}  else { return false;}
 }
 
 function tienenMismaLongitud(str1, str2) {
