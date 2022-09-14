ece2458ee3983608ee40dcfb6e78edd8f46b1dcd
unknown <nachomontiel16@gmail.com>
2022-09-14 10:53:31 -0300

    first commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -146,21 +146,21 @@ function agregarSimboloExclamacion(str) {
   // Agrega un símbolo de exclamación al final de la string "str" y devuelve una nueva string
   // Ejemplo: "hello world" pasaría a ser "hello world!"
   // Tu código: 
-  return 'hello ' + 'world'+'!';
+  return 'Soy ' + 'Henry'+'!';
 }
 
 function combinarNombres(nombre, apellido) {
   // Devuelve "nombre" y "apellido" combinados en una string y separados por un espacio.
   // Ejemplo: "Soy", "Henry" -> "Soy Henry"
   // Tu código: 
-  return 'Soy '+'Henry';
+  return 'hello '+'world';
 }
 
 function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
-  // "Martin" -> "Hola Martin!"
+  // "Martin" -> "Hola Antonio!"
   // Tu código: 
-  return 'Hola '+'Martin'+'!';
+  return 'Hola '+'Antonio'+'!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
