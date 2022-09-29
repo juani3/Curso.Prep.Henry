1a4d4a79dce1138bc43237c66c8255c724410612
unknown <nachomontiel16@gmail.com>
2022-09-29 01:30:38 -0300

    Update README.md

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -35,7 +35,7 @@ function estaEnRango(numero) {
   // Devuelve "true" si "numero" es menor que 50 y mayor que 20
   // De lo contrario, devuelve "false"
   // Tu código: 
-  if ((numero<50)&&(numero>20)) {return true;} 
+  if ( numero < 50 && numero > 20 ) {return true;} 
   else {return false;}
 }
 
